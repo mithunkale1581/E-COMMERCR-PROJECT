fc83b1c3ca953dd5e8df14c9150c7385c46ba81a
mithunkale1581 <mithunkale1581@gmail.com>
2025-01-21 19:54:02 +0530

    order file updated

--- client/src/pages/shopping-view/account.jsx
+++ client/src/pages/shopping-view/account.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import accImg from "../../assets/account.jpg";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Address from "@/components/shopping-view/address";
-import ShoppingOrders from "@/components/shopping-view/orders";
+import ShoppingOrders from "@/components/shopping-view/orders.jsx";
 
 const ShoppingAccount = () => {
   return (
